fe73c5b142925126259e20618ff30d0fbc475125
Junyu Gao <gjy3035@gmail.com>
2020-01-08 10:32:02 +0800

    VGG para

--- config.py
+++ config.py
@@ -14,25 +14,26 @@ __C.VIS = edict()
 
 #------------------------------DATA------------------------
 __C.DATA.STD_SIZE = (576,768)
-__C.DATA.DATA_PATH = '/media/D/DataSet/CC/' +str(__C.DATA.STD_SIZE[0]) + 'x' + str(__C.DATA.STD_SIZE[1]) + 'RGB/shanghaitech_part_B'                  
-__C.DATA.MEAN_STD = ([0.444637000561], [0.226200059056]) # part B
-__C.DATA.DEN_ENLARGE = 100.
+__C.DATA.DATA_PATH = '/media/E/own_open_source/ProcessedData/Data.2019.11/NWPU/mask_min_576x768_mod16_2048'                 
+__C.DATA.MEAN_STD = ([0.446139603853, 0.409515678883, 0.395083993673], [0.288205742836, 0.278144598007, 0.283502370119]) 
+__C.DATA.DEN_ENLARGE = 1.
 
 #------------------------------TRAIN------------------------
-__C.TRAIN.INPUT_SIZE = (512,680)
+__C.TRAIN.INPUT_SIZE = (576,768)
 __C.TRAIN.SEED = 640
 __C.TRAIN.RESUME = ''#model path
 __C.TRAIN.BATCH_SIZE = 6 #imgs
-__C.TRAIN.BCE_WIEGHT = 1e-2
+__C.TRAIN.BCE_WIEGHT = 1e-4
 
 __C.TRAIN.SEG_LR = 1e-5
-__C.TRAIN.SEG_WIEGHT = 1e-2
+__C.TRAIN.SEG_WIEGHT = 1e-4
 
 __C.TRAIN.NUM_BOX = 20 #boxes
 
-__C.TRAIN.GPU_ID = [1]
+__C.TRAIN.GPU_ID = [0]
 
 # base lr
+__C.TRAIN.VGG_LR = 1e-5
 __C.TRAIN.LR = 1e-4
 __C.TRAIN.LR_DECAY = 0.995
 __C.TRAIN.NUM_EPOCH_LR_DECAY = 1 # epoches
